9fa3542d4b8518b9fda237e3b6bd7a6796d7c509
chengyang1380 <18714826+chengyang1380@users.noreply.github.com>
2025-11-02 16:49:48 +0800

    Add RoleSelectionFeature unit tests and update scheme
    Added RoleSelectionFeatureTests.swift with tests for role selection and reset actions. Removed the placeholder FeatureTests.swift file. Introduced Features.xcscheme to configure build and test actions for the Features module.

---
+++ Feature/Tests/FeatureTests/RoleSelectionFeatureTests.swift
@@ -0,0 +1,58 @@
+import ComposableArchitecture
+import Features
+import Testing
+
+struct RoleSelectionFeatureTests {
+
+    @Test
+    func selectRole() async throws {
+        let store = await TestStore(
+            initialState: RoleSelectionFeature.State()
+        ) { RoleSelectionFeature() }
+
+        await store.send(.view(.selectRole(.merlin))) {
+            $0.selectedRoles = [
+                .percival,
+                .loyalServant,
+                .morgana,
+                .assassin,
+            ]
+        }
+
+        await store.send(.view(.selectRole(.merlin))) {
+            $0.selectedRoles = [
+                .merlin,
+                .percival,
+                .loyalServant,
+                .morgana,
+                .assassin,
+            ]
+        }
+    }
+
+    @Test
+    func resetButtonTapepd() async throws {
+        let store = await TestStore(
+            initialState: RoleSelectionFeature.State()
+        ) { RoleSelectionFeature() }
+
+        await store.send(.view(.selectRole(.merlin))) {
+            $0.selectedRoles = [
+                .percival,
+                .loyalServant,
+                .morgana,
+                .assassin,
+            ]
+        }
+
+        await store.send(.view(.resetButtonTapepd)) {
+            $0.selectedRoles = [
+                .merlin,
+                .percival,
+                .loyalServant,
+                .morgana,
+                .assassin,
+            ]
+        }
+    }
+}
