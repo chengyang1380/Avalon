e137db35e13226debf9ffdc0b3602b78fe3a5d4b
chengyang1380 <18714826+chengyang1380@users.noreply.github.com>
2026-02-13 00:59:39 +0800

    Add role dialogs, shuffling, and descriptions
    Introduce confirmation and role alert dialogs to RoleAssignmentFeature and implement core reducer logic. Convert Action to ViewAction with PresentationAction handling for confirmationDialog and roleDialog, add @Presents AlertState properties, and wire .ifLet scoping. Add a roleShuffler dependency and handle .view(.startTask) to shuffle roles. When a role cell is tapped, present a confirmation dialog and then a role dialog showing the selected role.
    
    Update Role model: make name and imageName public, fix translation for Oberon, and add a public roleDescription property with localized descriptions for each role.

--- Feature/Sources/Features/RoleAssignmentFeature.swift
+++ Feature/Sources/Features/RoleAssignmentFeature.swift
@@ -6,30 +6,98 @@
 //
 
 import ComposableArchitecture
+import DependencyClients
 import Models
 
 @Reducer
 public struct RoleAssignmentFeature {
+
     @ObservableState
     public struct State: Equatable {
         public var roles: IdentifiedArrayOf<Role>
+
+        @Presents
+        public var confirmationDialog: AlertState<Action.ConfirmationDialog>?
+
+        @Presents
+        public var roleDialog: AlertState<Action.RoleDialog>?
 
         public init(roles: IdentifiedArrayOf<Role>) {
             self.roles = roles
         }
     }
 
-    public enum Action: Equatable {
+    public enum Action: ViewAction {
+        case view(View)
+        case confirmationDialog(PresentationAction<ConfirmationDialog>)
+        case roleDialog(PresentationAction<RoleDialog>)
 
+        public enum View {
+            case startTask
+            case showRoleTapped(Int)
+        }
+
+        @CasePathable
+        public enum ConfirmationDialog: Equatable {
+            case showRoleTapped(Int)
+        }
+
+        @CasePathable
+        public enum RoleDialog {
+            case finish
+        }
     }
+
+    @Dependency(\.roleShuffler) var roleShuffler
 
     public init() {}
 
     public var body: some ReducerOf<Self> {
         Reduce(core)
+            .ifLet(\.$confirmationDialog, action: \.confirmationDialog)
+            .ifLet(\.$roleDialog, action: \.roleDialog)
     }
 
     public func core(state: inout State, action: Action) -> Effect<Action> {
-        return .none
+        switch action {
+        case .view(.startTask):
+            let shuffled = roleShuffler.shuffle(roles: state.roles.elements)
+            state.roles = IdentifiedArrayOf(uniqueElements: shuffled)
+            return .none
+
+        case .view(.showRoleTapped(let index)):
+            state.confirmationDialog = AlertState {
+                TextState("注意")
+            } actions: {
+                ButtonState(role: .cancel) {
+                    TextState("取消")
+                }
+                ButtonState(action: .showRoleTapped(index)) {
+                    TextState("確定")
+                }
+            } message: {
+                TextState("確定您是玩家\(index + 1)嗎？")
+            }
+            return .none
+
+        case .confirmationDialog(.presented(.showRoleTapped(let index))):
+            let role = state.roles[index]
+            state.roleDialog = AlertState {
+                TextState("您的角色是 - \(role.name)")
+            } actions: {
+                ButtonState(action: .finish) {
+                    TextState("確認")
+                }
+            } message: {
+                TextState(role.roleDescription)
+            }
+            return .none
+
+        case .confirmationDialog,
+            .roleDialog:
+            return .none
+
+        }
     }
 }
+
--- Feature/Sources/Models/Role.swift
+++ Feature/Sources/Models/Role.swift
@@ -24,8 +24,8 @@ public enum Role: String, Identifiable, Equatable, Sendable, CaseIterable {
     case mordred
 }
 
-public extension Role {
-     var name: String {
+extension Role {
+    public var name: String {
         switch self {
         case .merlin:
             return "梅林"
@@ -38,7 +38,7 @@ public extension Role {
         case .mordred:
             return "莫德雷德"
         case .oberon:
-            return "奧伯龍"
+            return "奧伯倫"
         case .minion,
             .minion1,
             .minion2,
@@ -52,7 +52,7 @@ public extension Role {
         }
     }
 
-    var imageName: String {
+    public var imageName: String {
         switch self {
         case .merlin:
             return "merlin"
@@ -76,6 +76,33 @@ public extension Role {
             .loyalServant2,
             .loyalServant3:
             return "loyalServant"
+        }
+    }
+
+    public var roleDescription: String {
+        switch self {
+        case .merlin:
+            return "好人：你可以看到所有壞人，除了大魔王 - 莫德雷德，記得要保護自己的身份，別太明顯，否則會被刺客殺掉"
+        case .assassin:
+            return "壞人：最後壞人要反敗要靠你了，可以選擇刺殺一個好人，刺殺到「梅林」，壞人就獲勝"
+        case .percival:
+            return "好人：西瓜皮，可以看到真假梅林（梅林 or 莫甘娜），努力領導好人是你的職責"
+        case .morgana:
+            return "壞人：壞梅林，表面上看起來和梅林一模一樣，讓西瓜皮（派西維爾）搞不清楚誰是真梅林，是你的優勢也是你的弱點"
+        case .mordred:
+            return "壞人：大魔王，梅林看不到你，利用這點來混淆梅林吧！"
+        case .oberon:
+            return "壞人：梅林知道你，但所有壞人都不知道你是壞人，你也不知道其他壞人是誰，獨立行動吧！"
+        case .minion,
+            .minion1,
+            .minion2,
+            .minion3:
+            return "壞人：邪惡僕從，沒有特殊能力，努力與其他壞人狼狽為奸，讓壞人獲勝吧！"
+        case .loyalServant,
+            .loyalServant1,
+            .loyalServant2,
+            .loyalServant3:
+            return "好人：忠誠僕從，沒有特殊能力，努力配合梅林和派西維爾，讓好人獲勝吧！"
         }
     }
 }
